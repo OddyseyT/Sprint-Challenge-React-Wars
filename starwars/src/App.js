9460879ad15bd927fa1dc49ebfecd4dc08f99b14
Lambda_School_Loaner_282 <lambda_school_loaner_282@Lambda-School-Loaner-282s-MacBook-Pro.local>
2020-05-31 20:47:34 -0500

    First push.  Got the cards set up

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {Label, Input } from "reactstrap";
+import Character from "./components/Character"
 
 const App = () => {
+  const [char, setChar] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  useEffect(() => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+    axios.get("https://swapi.py4e.com/api/people/")
+   .then(response => {
+     console.log(response.data.results)
+     setChar(response.data.results)
+   })
+    .catch(err => {console.log("something went wrong", err)})
 
+.then(() => {console.log("success")})
+}, []);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+<Character characterArray ={char}/>
     </div>
+
   );
-}
+  }
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React from "react";
+import { Container, Col, Row, Card, CardHeader, CardFooter, CardBody,
+ CardText } from 'reactstrap';
+
+ function Character (props) {
+    console.log(props.characterArray)
+  
+      return(
+          <Container>
+            <Row>
+          {props.characterArray.map(character => {
+           return(    
+  
+  <Col xs="3">
+  <Card key = {character.created} >
+        <CardHeader>{character.name}</CardHeader>
+        <CardBody>
+ 
+          <CardText>{`Born: ${character.birth_year}`}</CardText>
+          <CardText>{character.gender}</CardText>
+          <CardText>{`Created: ${character.created}`}</CardText>
+        </CardBody>
+        <CardFooter>
+        </CardFooter>
+      </Card>
+  </Col>)})}
+  </Row>
+          </Container>
+           );}
+   
+  
+  export default Character;
